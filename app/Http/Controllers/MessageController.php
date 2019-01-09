54ff8fcf77488f3a6e3f9f439745932e6b5f3677
deivyrene <deivyhernandezperez@gmail.com>
2019-01-09 10:42:20 -0300

    Subiendo

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+use App\Events\MessageSent;
+
+class MessageController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function fetchMessages()
+    {
+        return Message::with('user')->get();
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $message = auth()->user()->messages()->create(['message'=>$request->message]);
+
+        broadcast(new MessageSent(auth()->user(),$message->load('user')))->toOthers();
+        
+        return response(['status'=>'Mensaje enviado satisfactoriamente']);
+    }
+}
---
+++ resources/js/components/Chat.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card class="chat-card" color="white">
+        <v-list>
+          <v-subheader>
+              Chat Privado
+            </v-subheader>
+            <v-divider></v-divider>
+            <v-list-tile
+              class="p-3"
+              v-for="(message, index) in allMessages" 
+              :key="index"
+            >
+
+           <v-layout 
+           :align-end="(user.id!==message.user.id)" 
+            column
+           >
+             <v-flex>
+               <v-layout column>
+                  <v-flex>
+                    <span class="small font-italic">{{message.user.name}}</span>
+                  </v-flex>
+
+                  <v-flex>
+                    <v-chip
+                      :color="(user.id!==message.user.id)?'red':'green'"
+                      text-color="white"
+                    >
+
+                      <v-list-tile-content >
+                       {{message.message}}
+                      </v-list-tile-content>
+                    </v-chip>
+                  </v-flex>
+
+                  <v-flex class="caption font-italic">
+                    {{message.created_at}}
+                  </v-flex>
+               </v-layout>
+             </v-flex>
+           </v-layout>
+
+
+            </v-list-tile>
+        </v-list>
+      </v-card>
+
+    </v-flex>
+    
+      <v-footer
+      height="auto"
+      fixed
+      color="grey"
+      >
+      <v-layout row >
+        <v-flex xs6 offset-xs3 justify-center align-center>
+            <v-text-field
+              rows=2
+              v-model="message"
+              label="Ingrese mensaje"
+              single-line
+              @keyup.enter="sendMessage"
+            ></v-text-field>
+        </v-flex>
+        
+        <v-flex xs2> 
+            <v-btn 
+                @click="sendMessage"
+                dark class="mt-3 ml-2 white--text" small color="black">adjuntar</v-btn>
+            <v-btn 
+                @click="sendMessage"
+                dark class="mt-3 ml-2 white--text" small color="green">enviar</v-btn>
+        </v-flex>
+      </v-layout>
+      
+          
+    </v-footer>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+
+  props: ['user'],
+  data(){
+
+    return {
+      message: null,
+      allMessages: []
+    }
+
+  },
+
+  methods:{
+
+    sendMessage(){
+
+      if(!this.message){
+        return alert('Ingrese un mensaje');
+      }
+
+      //this.allMessages.push(this.message);
+
+      axios.post('/messages', {message: this.message}).then(response => {
+              console.log(response.data);
+              this.message=null;
+              this.fetchMessages();
+              setTimeout(this.scrollToEnd,100);
+      });
+
+    },
+
+    fetchMessages() {
+
+      axios.get('/messages').then(response => {
+              this.allMessages = response.data;
+      });
+
+    },
+
+    scrollToEnd(){
+        window.scrollTo(0,99999);
+      }
+
+  },
+
+  mounted(){
+    
+  },
+
+  created(){
+    this.fetchMessages();
+
+    Echo.private('lchat')
+    .listen('MessageSent',(e)=>{
+        this.allMessages.push(e.message)
+        setTimeout(this.scrollToEnd,100);
+    });
+  }
+
+}
+  
+</script>
+
+<style scoped>
+.chat-card{
+  margin-bottom:140px;
+}
+</style>
